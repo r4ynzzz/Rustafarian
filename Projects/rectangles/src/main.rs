2ee8f1414ba2233f349275e9d1083f6efb576b76
r4ynzzz <ryanttncube@gmail.com>
2026-06-06 09:33:09 +0200

    structs and debugs

--- Projects/rectangles/src/main.rs
+++ Projects/rectangles/src/main.rs
@@ -1,13 +1,25 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
 
-    println!(
+    /*println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
-    );
+        area(&rect1)
+    );*/
+    //println!("rect1 is {rect1:#?}")
+
+    dbg!(&rect1);
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
-
