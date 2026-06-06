0a909f93507acc780cd0b51baa00780764e057bc
r4ynzzz <ryanttncube@gmail.com>
2026-06-06 09:54:57 +0200

    method beginning

--- Projects/rectangles/src/main.rs
+++ Projects/rectangles/src/main.rs
@@ -4,10 +4,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 2;
+    //let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30, //dbg!(30 * scale),
         height: 50,
     };
 
@@ -17,9 +23,16 @@ fn main() {
     );*/
     //println!("rect1 is {rect1:#?}")
 
-    dbg!(&rect1);
+    //dbg!(&rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    )
 }
 
+/*
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 }
+*/
