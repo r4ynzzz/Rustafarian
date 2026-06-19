7edded1366f8f82c893eec4b994f75e59f155e43
r4ynzzz <ryanttncube@gmail.com>
2026-06-19 09:53:37 +0200

    let...else contol

--- let_else_control/src/main.rs
+++ let_else_control/src/main.rs
@@ -1,3 +1,45 @@
+use std::fmt::format;
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Georgia,
+    California,
+    Texas,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Georgia => year >= 1819,
+            UsState::Texas => year >= 1959,
+            UsState::California => year >= 1969,
+            // -- snip --
+        }
+    }
+}
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return None;
+    };
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America!"))
+    } else {
+        Some(format!("{state:?} is relatively new."))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let b = describe_state_quarter(Coin::Quarter(UsState::Texas));
+
+    println!("{b:?}");
 }
