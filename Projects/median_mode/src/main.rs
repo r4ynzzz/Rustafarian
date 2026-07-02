dcc253e5056cb01a88919a9c79a44763f15723dc
r4ynzzz <ryanttncube@gmail.com>
2026-07-02 12:20:20 +0200

    mode completed

--- Projects/median_mode/src/main.rs
+++ Projects/median_mode/src/main.rs
@@ -17,21 +17,30 @@ fn median(numbers: &mut [i32]) -> Option<f64> {
     }
 }
 
-fn mode(numbers: &[i32]) -> Option<i32> {
-    if numbers.is_empty() {
-        return None;
+fn mode(numbers: &[i32]) -> Vec<i32> {
+    let mut counts = HashMap::new();
+
+    //1. Count occurrences
+    for &num in numbers {
+        *counts.entry(num).or_insert(0) += 1;
     }
 
-    let mut map = HashMap::new();
+    //2. Find the highest frequency
+    let max_count = counts.values().cloned().max().unwrap_or(0);
 
-    for n in numbers {
-        let count = map.entry(n).or_insert(0);
-        *count += 1;
-    }
+    //Collect all values that match highest frequency
+    let modes: Vec<i32> = counts
+        .into_iter()
+        .filter(|&(_, count)| count == max_count)
+        .map(|(num, _)| num)
+        .collect();
+
+    modes
 }
 
 fn main() {
     let mut numbers = vec![19, 12, 43, 64, 25, 16, 97, 58, 22, 10, 43];
 
-    println!("Median: {:?}", median(&mut numbers))
+    println!("Mode: {:?}", mode(&numbers));
+    println!("Median: {:?}", median(&mut numbers));
 }
